343cb8f61186ff99979884289e30c3854230a80d
jiangguoqing <mrjiangguoqing@gmail.com>
2023-06-18 20:35:00 +0800

    test

--- vpc/_example/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = module.vpc.*.vpc_id
-  description = "The ID of the VPC."
-}
-
-output "tags" {
-  value       = module.vpc.*.tags
-  description = "A mapping of tags to assign to the resource."
-}
